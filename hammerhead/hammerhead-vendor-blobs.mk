aa7b2c657278da49fa8defb2a08d9d6dfdaf3f3a
myfluxi <linflux@arcor.de>
2015-04-12 00:30:54 +0200

    hammerhead: Update mpdecision, use libqti-perfd-client
    Change-Id: I5159ecb8a80deb2f3693351b71cfae5f6972ae97

--- hammerhead/hammerhead-vendor-blobs.mk
+++ hammerhead/hammerhead-vendor-blobs.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/hammerhead/proprietary/bin/mpdecision:system/bin/mpdecision \
     vendor/lge/hammerhead/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/lge/hammerhead/proprietary/bin/nl_listener:system/bin/nl_listener \
+    vendor/lge/hammerhead/proprietary/bin/perfd:system/bin/perfd \
     vendor/lge/hammerhead/proprietary/bin/port-bridge:system/bin/port-bridge \
     vendor/lge/hammerhead/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/lge/hammerhead/proprietary/bin/qseecomd:system/bin/qseecomd \
@@ -196,6 +197,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/hammerhead/proprietary/vendor/lib/liboemcamera.so:system/vendor/lib/liboemcamera.so \
     vendor/lge/hammerhead/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
     vendor/lge/hammerhead/proprietary/vendor/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so \
+    vendor/lge/hammerhead/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so \
     vendor/lge/hammerhead/proprietary/vendor/lib/libqdi.so:system/vendor/lib/libqdi.so \
     vendor/lge/hammerhead/proprietary/vendor/lib/libqdp.so:system/vendor/lib/libqdp.so \
     vendor/lge/hammerhead/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
